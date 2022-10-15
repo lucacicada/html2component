e3403d89ed2cc1e9ae9635224cce0d6374752dc7
lucacicada <100947658+lucacicada@users.noreply.github.com>
2022-10-15 10:04:02 +0200

    Publish playground build

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -2,5 +2,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[hash].js',
+        chunkFileNames: 'assets/[hash].js',
+        assetFileNames: 'assets/[hash].[ext]'
+      }
+    }
+  }
 })
